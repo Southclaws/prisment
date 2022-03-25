e23916575c87157ede27b03a3204ac3a258da45a
ADRFranklin <adrfranklin95@gmail.com>
2022-03-25 21:24:00 +0000

    chore: cleaned up functions and added a fixed default value for json fields

--- convert.ts
+++ convert.ts
@@ -1,13 +1,35 @@
 import { DMMF } from "@prisma/generator-helper";
 import { getDMMF } from "@prisma/sdk";
 import { pascalCase, snakeCase } from "change-case";
-import { fstat } from "fs";
 import { mkdir, writeFile } from "fs/promises";
 
 type Schema = {
   path: string;
   source: string;
 };
+export interface DatamodelFieldMap {
+  model: DMMF.Model;
+  enums: DMMF.Field[];
+  scalars: DMMF.Field[];
+  relations: DMMF.Field[];
+}
+export interface DatamodelStructure {
+  enumMap: Record<string, DMMF.Model>;
+  modelMap: Record<string, DMMF.Model>;
+  modelFieldMap: Record<string, DatamodelFieldMap>;
+}
+
+const PrismaTypesMap = new Map([
+  ["String", "String"],
+  ["Boolean", "Bool"],
+  ["Int", "Int"],
+  ["BigInt", "Int64"],
+  ["Float", "Float"],
+  ["Decimal", "Float"],
+  ["DateTime", "Time"],
+  ["Json", "JSON"],
+  ["Bytes", "Bytes"],
+]);
 
 const slice = (l: string[]) => [...l, "\n    "].join(",");
 const quotedSlice = (l: string[]) => l.map((v) => `"${v}"`).join(",");
@@ -50,17 +72,86 @@ const defaultValue = (f: DMMF.Field) => {
   }
 };
 
-const getFieldType = (f: DMMF.Field) => {
+export function getModelFieldMap(model: DMMF.Model): DatamodelFieldMap {
+  const fields = model.fields;
+  const enums = fields.filter((f) => f.kind === "enum");
+  const scalars = fields.filter((f) => f.kind === "scalar");
+  const relations = fields.filter((f) => f.kind === "object");
+
+  const relationFieldIs: any = [];
+  relations.forEach((item) => {
+    if (item.relationFromFields?.length) {
+      relationFieldIs.push(...item.relationFromFields);
+    }
+  });
+
+  return {
+    model,
+    enums,
+    scalars: scalars.filter((scalar) => !relationFieldIs.includes(scalar.name)),
+    relations,
+  };
+}
+
+export function buildDatamodelStructure(
+  dmmf: DMMF.Document
+): DatamodelStructure {
+  const enumMap = dmmf.datamodel.enums.reduce(
+    (acc, curr) => ({ ...acc, [curr.name]: curr }),
+    {}
+  );
+  const modelMap = dmmf.datamodel.models.reduce(
+    (acc, curr) => ({ ...acc, [curr.name]: curr }),
+    {}
+  );
+  const modelFieldMap = dmmf.datamodel.models.reduce(
+    (acc, curr) => ({ ...acc, [curr.name]: getModelFieldMap(curr) }),
+    {}
+  );
+
+  return {
+    enumMap,
+    modelMap,
+    modelFieldMap,
+  };
+}
+
+const getFieldValue = (f: DMMF.Field, d: DatamodelFieldMap): string => {
   const t = f.type;
   switch (t) {
-    case "Boolean":
-      return "Bool";
-    case "DateTime":
-      return "Time";
-
-    default:
-      return pascalCase(t);
+    case "Json": {
+      return "[]string{}";
+    }
+    default: {
+      return "";
+    }
   }
+};
+
+interface Field {
+  Type: string;
+  Name: string;
+  Value: string;
+  Suffix: string;
+}
+
+const getField = (f: DMMF.Field, d: DatamodelFieldMap): Field | undefined => {
+  const type = PrismaTypesMap.get(f.type);
+  if (type === undefined) {
+    return undefined;
+  }
+
+  if (f.relationToFields) {
+    return undefined;
+  }
+
+  const values = getFieldValue(f, d);
+  return {
+    Type: type,
+    Name: `"${f.name}"`,
+    Value: values ? ", " + values : "",
+    Suffix: getSuffixes(f) || "",
+  };
 };
 
 const getSuffixes = (f: DMMF.Field) =>
@@ -71,52 +162,49 @@ const getSuffixes = (f: DMMF.Field) =>
     ...defaultValue(f),
   ].join("");
 
-const enumToGo = (f: DMMF.Field, d: DMMF.Document): string => {
+const enumToGo = (f: DMMF.Field, structure: DatamodelStructure): string => {
   const suffixes = getSuffixes(f);
-
-  const enumData = d.datamodel.enums
-    .filter((e) => e.name == f.type)
-    .map((e) => e.values);
-  const enumValues = enumData[0].map((e) => e.name);
-
+  const values = structure.enumMap[f.type].values.map((v: any) => v.name);
   return `
-        field.Enum("${f.name}").Values(${quotedSlice(enumValues)})${suffixes}`;
+        field.Enum("${f.name}").Values(${quotedSlice(values)})${suffixes}`;
 };
 
-const fieldToGo = (f: DMMF.Field, d: DMMF.Document): string | undefined => {
+const fieldToGo = (
+  f: DMMF.Field,
+  data: DatamodelFieldMap,
+  structure: DatamodelStructure
+): string | undefined => {
   if (f.kind === "enum") {
-    return enumToGo(f, d);
+    return enumToGo(f, structure);
   }
 
-  const fieldType = getFieldType(f);
-
-  const suffixes = getSuffixes(f);
-
-  if (f.relationToFields) {
+  const field = getField(f, data);
+  if (field === undefined) {
     return undefined;
   }
 
   return `
-        field.${fieldType}("${f.name}")${suffixes}`;
+        field.${field.Type}(${field.Name}${field.Value})${field.Suffix}`;
 };
 
 const edgeToGo = (f: DMMF.Field): string => {
   //console.log(f.name, f.relationFromFields, f.relationName, f.relationToFields);
   return `
-    edge.To("${f.name}", ${f.type}.Type)`;
+        edge.To("${f.name}", ${f.type}.Type)`;
 };
 
-const modelToGo = (model: DMMF.Model, document: DMMF.Document): Schema => {
-  const { name, fields } = model;
-
+const modelToGo = (
+  data: DatamodelFieldMap,
+  structure: DatamodelStructure
+): Schema => {
+  const { name, fields } = data.model;
   const path = `./ent/schema/${snakeCase(name)}.go`;
 
   const edges = fields.filter((v) => v.relationToFields !== undefined);
-
   const edgeList = edges.map(edgeToGo);
 
   const fieldList: string[] = fields
-    .map((f) => fieldToGo(f, document))
+    .map((f) => fieldToGo(f, data, structure))
     .filter((v) => v !== undefined) as string[];
 
   // TODO: generate a list of relationships
@@ -166,8 +254,11 @@ const save = async (s: Schema) => {
 
 (async () => {
   const dmmf = await getDMMF({ datamodelPath: "prisma/schema.prisma" });
+  const structure: DatamodelStructure = buildDatamodelStructure(dmmf);
 
-  const models = dmmf.datamodel.models.map((model) => modelToGo(model, dmmf));
+  const models = dmmf.datamodel.models.map((model) =>
+    modelToGo(structure.modelFieldMap[model.name], structure)
+  );
 
   // TODO: Invoke `go generate`
 
